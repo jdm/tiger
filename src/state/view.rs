416762d6af4e943b2b39363ea6e26182344bbbf8
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-07 13:56:11 -0700

    Adjust min/max zoom levels

--- src/state/view.rs
+++ src/state/view.rs
@@ -58,7 +58,7 @@ impl View {
         } else {
             self.workbench_zoom_level /= 2;
         }
-        self.workbench_zoom_level = std::cmp::min(self.workbench_zoom_level, 16);
+        self.workbench_zoom_level = std::cmp::min(self.workbench_zoom_level, 32);
     }
 
     pub fn workbench_zoom_out(&mut self) {
@@ -69,7 +69,7 @@ impl View {
         } else {
             self.workbench_zoom_level *= 2;
         }
-        self.workbench_zoom_level = std::cmp::max(self.workbench_zoom_level, -8);
+        self.workbench_zoom_level = std::cmp::max(self.workbench_zoom_level, -4);
     }
 
     pub fn workbench_reset_zoom(&mut self) {
