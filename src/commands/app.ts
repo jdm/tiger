395da081674d630da0167004293a56ea97c1e6d5
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-10 21:59:29 -0700

    Added TODO

--- src/commands/app.ts
+++ src/commands/app.ts
@@ -1,11 +1,18 @@
 import { useAppStore, AppState } from "@/stores/app";
 import { invoke } from "@tauri-apps/api/tauri";
 
+// TODO Replacing store.$state with command output will trigger reactive
+// changes througout the whole UI.
+// Consider making commands return a patch structure so that only the
+// relevant store fields get modified.
+// Possible solution:
+// - JS https://github.com/martindale/fast-json-patch
+// - Rust https://github.com/idubrov/json-patch
+
 export async function openDocument(path: String): Promise<void> {
   const app: AppState = await invoke("open_document", {
     path: path,
   });
   const appStore = useAppStore();
   appStore.$state = app;
-  // TODO this also also needs to update the document store
 }
