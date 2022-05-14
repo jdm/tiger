4615f0cc39aa9a4065891784887715d5f3baf53f
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-14 14:12:17 -0700

    Style tweak

--- src/components/tabs/Tab.vue
+++ src/components/tabs/Tab.vue
@@ -1,6 +1,6 @@
 <template>
 	<li @click="$emit('select')" @mouseover="onMouseOver" @mouseout="onMouseOut"
-		class="inline-block py-3 px-3 cursor-pointer" :class="selected ? activeClasses : inactiveClasses">
+		class="inline-block py-3 px-3 pb-1 cursor-pointer" :class="selected ? activeClasses : inactiveClasses">
 		<slot></slot>
 		<XIcon @click="$emit('close')" :class="(selected || hovered) ? 'visible' : 'invisible'"
 			class="inline h-7 w-7 p-1 ml-2 rounded-lg hover:bg-neutral-800 hover:visible" />
