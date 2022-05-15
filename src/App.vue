8fd90c3c4b5ffde108cfa7e89f2917d7694f843e
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-15 00:26:28 -0700

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
+import { useAppStore } from '@/stores/app'
+import { openFiles } from '@/api/local'
+import Workbench from '@/components/Workbench.vue';
+import ContentPane from '@/components/ContentPane.vue'
+
+const app = useAppStore()
+
 </script>
 
 <template>
-  <HelloWorld />
+
+  <div class="flex flex-nowrap flex-col h-screen w-screen overflow-clip select-none">
+
+    <button @click="openFiles" class="place-self-start">Open document</button>
+
+    <div class="flex-1 min-h-0 flex flex-row">
+      <ContentPane class="basis-96 min-w-0" />
+      <Workbench class="flex-1" />
+    </div>
+    <div class="p-4 basis-60 bg-stone-800">
+      Beep boop I'm a timeline
+    </div>
+  </div>
 </template>
