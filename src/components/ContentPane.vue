8722a5b91e9515ba78dd8f2cd6934aa65fb28516
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-15 16:21:15 -0700

    Image previews in frame panel

--- src/components/ContentPane.vue
+++ src/components/ContentPane.vue
@@ -5,17 +5,15 @@
 			<PaneTab @select="focusContentTab('animations')" :selected="currentTab == 'animations'">Animations</PaneTab>
 		</template>
 		<template #content>
-			<div v-if="currentTab == 'frames'">
+			<div v-if="currentTab == 'frames'" class="grid grid-cols-4 gap-4 m-4">
 				<div v-for="frame in app.currentDocument?.sheet.frames" @click="(event) => onFrameClicked(frame, event)"
-					class="p-2 border-b last:border-0 border-gray-700 pl-4 overflow-x-hidden text-ellipsis hover:bg-gray-700"
-					:class="frame.selected ? 'text-sky-400 bg-gray-900' : ''">
-					<div class="flex flex-row">
-						<PhotographIcon class="self-center w-6 h-6" />
-						<div class="px-4 flex flex-col">
-							<div class="">{{ frame.name }}</div>
-							<div class="text-xs text-gray-400">64x64 px</div>
-						</div>
+					class="flex flex-col rounded-sm cursor-pointer"
+					:class="frame.selected ? 'outline outline-4 outline-blue-500' : ''">
+					<div class="flex place-content-center aspect-square checkerboard rounded-sm overflow-hidden">
+						<img :src="convertFileSrc(frame.path)" class="pixelated object-none" />
 					</div>
+					<div class="text-xs p-1 overflow-hidden text-ellipsis"
+						:class="frame.selected ? 'bg-blue-500 text-white' : 'text-zinc-400'">{{ frame.name }}</div>
 				</div>
 			</div>
 			<div v-if="currentTab == 'animations'">
@@ -42,6 +40,7 @@
 </template>
 
 <script setup lang="ts">
+import { convertFileSrc } from '@tauri-apps/api/tauri'
 import { useAppStore } from '@/stores/app'
 import { computed } from '@vue/reactivity';
 import { FilmIcon, PlusIcon, PhotographIcon } from '@heroicons/vue/outline'
@@ -65,3 +64,19 @@ function onAnimationClicked(animation: Animation, event: MouseEvent) {
 }
 
 </script>
+
+<style scoped>
+.pixelated {
+	image-rendering: pixelated;
+}
+
+.checkerboard {
+	background-size: 16px 16px;
+	background-image:
+		linear-gradient(45deg, theme('colors.neutral.700') 25%, transparent 25%, transparent 75%, theme('colors.neutral.700') 75%, theme('colors.neutral.700') 100%),
+		linear-gradient(45deg, theme('colors.neutral.700') 25%, theme('colors.neutral.600') 25%, theme('colors.neutral.600') 75%, theme('colors.neutral.700') 75%, theme('colors.neutral.700') 100%);
+	background-position:
+		0px 0px,
+		8px 8px;
+}
+</style>
