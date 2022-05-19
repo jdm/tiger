3d6296fa73d76bbbe640d134822c0fbba42bfea8
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-19 01:06:19 -0700

    Cosmetic change

--- src/api/app.ts
+++ src/api/app.ts
@@ -3,36 +3,20 @@ import { invoke } from "@tauri-apps/api";
 
 export async function newDocument(path: string): Promise<void> {
   const appStore = useAppStore();
-  appStore.patch(
-    await invoke("new_document", {
-      path: path,
-    })
-  );
+  appStore.patch(await invoke("new_document", { path: path }));
 }
 
 export async function openDocuments(paths: string[]): Promise<void> {
   const appStore = useAppStore();
-  appStore.patch(
-    await invoke("open_documents", {
-      paths: paths,
-    })
-  );
+  appStore.patch(await invoke("open_documents", { paths: paths }));
 }
 
 export async function focusDocument(path: string): Promise<void> {
   const appStore = useAppStore();
-  appStore.patch(
-    await invoke("focus_document", {
-      path: path,
-    })
-  );
+  appStore.patch(await invoke("focus_document", { path: path }));
 }
 
 export async function closeDocument(path: string): Promise<void> {
   const appStore = useAppStore();
-  appStore.patch(
-    await invoke("close_document", {
-      path: path,
-    })
-  );
+  appStore.patch(await invoke("close_document", { path: path }));
 }
