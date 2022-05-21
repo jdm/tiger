a1ee865d9136fa2794d1dff00dbcb04b6accf20c
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-20 23:45:02 -0700

    Timeline styling

--- src/components/timeline/TimelinePane.vue
+++ src/components/timeline/TimelinePane.vue
@@ -8,33 +8,41 @@
 		</div>
 		<PaneInset class="flex-1 m-4 mt-2 ">
 			<div class="relative flex flex-row h-full">
-				<div class="flex flex-col">
-					<div class="h-8 bg-plastic-900" />
+				<div class="flex flex-col bg-plastic-700">
+					<div class="h-8 bg-plastic-600" />
 					<div
-						class="w-40 flex-initial flex flex-col bg-plastic-900 pt-1 space-y-px divide-plastic-800 text-plastic-300 text-right">
-						<div class="h-8 bg-plastic-500 px-4">North</div>
-						<div class="h-8 bg-plastic-500 px-4">North East</div>
-						<div class="h-8 bg-plastic-500 px-4">East</div>
-						<div class="h-8 bg-plastic-500 px-4">South East</div>
-						<div class="h-8 bg-plastic-500 px-4">South</div>
-						<div class="h-8 bg-plastic-500 px-4">South West</div>
-						<div class="h-8 bg-plastic-500 px-4">West</div>
-						<div class="h-8 bg-plastic-500 px-4">North West</div>
+						class="w-40 flex-initial flex flex-col py-2 space-y-1 divide-plastic-800 text-plastic-300 text-xs uppercase font-semibold text-right">
+						<div class="h-8 px-4 inline-flex items-center justify-end">North</div>
+						<div class="h-8 px-4 inline-flex items-center justify-end">North East</div>
+						<div class="h-8 px-4 inline-flex items-center justify-end">East</div>
+						<div class="h-8 px-4 inline-flex items-center justify-end">South East</div>
+						<div class="h-8 px-4 inline-flex items-center justify-end">South</div>
+						<div class="h-8 px-4 inline-flex items-center justify-end">South West</div>
+						<div class="h-8 px-4 inline-flex items-center justify-end">West</div>
+						<div class="h-8 px-4 inline-flex items-center justify-end">North West</div>
 					</div>
 				</div>
-				<div class="flex-1 pl-1 flex flex-col relative overflow-x-scroll styled-scrollbars">
-					<div class="h-8 w-[2800px] self-stretch ruler" />
-					<div class="flex-1 flex flex-col min-w-0 pt-1 space-y-px">
-						<div class="h-8 flex flex-row min-w-fit bg-plastic-700">
+				<div class="flex-1 flex flex-col relative overflow-x-scroll styled-scrollbars">
+					<div class="h-8 w-[2800px] px-1 self-stretch ruler" />
+					<div class="flex-1 flex flex-col min-w-0 py-2 space-y-1 bg-plastic-700">
+						<div
+							class="w-full h-9 flex flex-row p-1 rounded-sm bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600">
 							<Keyframe v-for="keyframe in app.currentAnimation?.timeline" :keyframe="keyframe" />
 						</div>
-						<div class="w-full bg-plastic-700 h-8" />
-						<div class="w-full bg-plastic-700 h-8" />
-						<div class="w-full bg-plastic-700 h-8" />
-						<div class="w-full bg-plastic-700 h-8" />
-						<div class="w-full bg-plastic-700 h-8" />
-						<div class="w-full bg-plastic-700 h-8" />
-						<div class="w-full bg-plastic-700 h-8" />
+						<div
+							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+						<div
+							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+						<div
+							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+						<div
+							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+						<div
+							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+						<div
+							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+						<div
+							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
 					</div>
 					<div class="absolute top-0 left-[200px] h-full w-px bg-white" />
 				</div>
@@ -56,8 +64,6 @@ const app = useAppStore()
 <style scoped>
 .ruler {
 	background:
-		/* Hide initial tick*/
-		linear-gradient(90deg, theme('colors.plastic.600') 1px, transparent 1px),
 		/* 1s ticks */
 		linear-gradient(90deg, theme('colors.plastic.400') 1px, transparent 1px) left bottom repeat-x,
 		/* 100ms ticks */
@@ -66,8 +72,8 @@ const app = useAppStore()
 		linear-gradient(90deg, theme('colors.plastic.400') 1px, transparent 1px) left bottom repeat-x,
 		/* Solig BG */
 		theme('colors.plastic.600') repeat-x;
+	background-origin: content-box;
 	background-size:
-		100% 100%,
 		1000px 100%,
 		100px 10px,
 		10px 4px,
