83b1406e89d3479a1775c510dfe1ad0faa17fc4b
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-20 23:52:24 -0700

    Timeline styling

--- src/components/timeline/TimelinePane.vue
+++ src/components/timeline/TimelinePane.vue
@@ -9,40 +9,42 @@
 		<PaneInset class="flex-1 m-4 mt-2 ">
 			<div class="relative flex flex-row h-full">
 				<div class="flex flex-col bg-plastic-700">
-					<div class="h-8 bg-plastic-600" />
+					<div class="h-6 bg-plastic-600" />
 					<div
-						class="w-40 flex-initial flex flex-col py-2 space-y-1 divide-plastic-800 text-plastic-300 text-xs uppercase font-semibold text-right">
-						<div class="h-8 px-4 inline-flex items-center justify-end">North</div>
-						<div class="h-8 px-4 inline-flex items-center justify-end">North East</div>
-						<div class="h-8 px-4 inline-flex items-center justify-end">East</div>
-						<div class="h-8 px-4 inline-flex items-center justify-end">South East</div>
-						<div class="h-8 px-4 inline-flex items-center justify-end">South</div>
-						<div class="h-8 px-4 inline-flex items-center justify-end">South West</div>
-						<div class="h-8 px-4 inline-flex items-center justify-end">West</div>
-						<div class="h-8 px-4 inline-flex items-center justify-end">North West</div>
+						class="w-36 flex-initial flex flex-col py-2 space-y-1 divide-plastic-800 text-plastic-300 text-xs uppercase font-semibold text-right">
+						<div class="h-9 px-4 mx-2 inline-flex items-center justify-end text-amber-500 border-blue-600">
+							North
+						</div>
+						<div class="h-9 px-4 inline-flex items-center justify-end">North East</div>
+						<div class="h-9 px-4 inline-flex items-center justify-end">East</div>
+						<div class="h-9 px-4 inline-flex items-center justify-end">South East</div>
+						<div class="h-9 px-4 inline-flex items-center justify-end">South</div>
+						<div class="h-9 px-4 inline-flex items-center justify-end">South West</div>
+						<div class="h-9 px-4 inline-flex items-center justify-end">West</div>
+						<div class="h-9 px-4 inline-flex items-center justify-end">North West</div>
 					</div>
 				</div>
 				<div class="flex-1 flex flex-col relative overflow-x-scroll styled-scrollbars">
-					<div class="h-8 w-[2800px] px-1 self-stretch ruler" />
+					<div class="h-6 w-[2800px] px-1 self-stretch ruler" />
 					<div class="flex-1 flex flex-col min-w-0 py-2 space-y-1 bg-plastic-700">
 						<div
 							class="w-full h-9 flex flex-row p-1 rounded-sm bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600">
 							<Keyframe v-for="keyframe in app.currentAnimation?.timeline" :keyframe="keyframe" />
 						</div>
 						<div
-							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+							class="w-full h-9 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
 						<div
-							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+							class="w-full h-9 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
 						<div
-							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+							class="w-full h-9 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
 						<div
-							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+							class="w-full h-9 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
 						<div
-							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+							class="w-full h-9 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
 						<div
-							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+							class="w-full h-9 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
 						<div
-							class="w-full h-8 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
+							class="w-full h-9 rounded-sm p-1 bg-plastic-800 border-y border-t-plastic-900 border-b-plastic-600" />
 					</div>
 					<div class="absolute top-0 left-[200px] h-full w-px bg-white" />
 				</div>
