ab76bee25faa065ff7191b11d8992cc066985890
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-25 00:04:55 -0700

    Cleanup

--- src/components/basic/WindowTitleBar.vue
+++ src/components/basic/WindowTitleBar.vue
@@ -6,13 +6,13 @@
 		</div>
 		<div class="flex flex-row px-5">
 			<slot name="right" />
-			<div class="flex-1 flex flex-row">
+			<div class="flex-1 flex flex-row text-plastic-300">
 				<button @click="appWindow.minimize" class="h-full p-2 px-6 hover:bg-plastic-600">➖</button>
 				<button v-if="isMaximized" @click="appWindow.unmaximize"
 					class="h-full p-2 px-6 hover:bg-plastic-600">🚸</button>
 				<button v-if="!isMaximized" @click="appWindow.maximize"
 					class="h-full p-2 px-6 hover:bg-plastic-600">⬜</button>
-				<XIcon @click="appWindow.close" class="h-full p-2 px-6 hover:bg-red-700" />
+				<XIcon @click="appWindow.close" class="h-full p-2 px-6 hover:bg-red-700 hover:text-plastic-100" />
 			</div>
 		</div>
 	</div>
