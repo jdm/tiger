3b67f9a0f19cbe43be7248ed23c2d0a342e63c03
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-28 20:06:40 -0700

    Minor visual tweaks

--- src/components/content/Frames.vue
+++ src/components/content/Frames.vue
@@ -1,16 +1,14 @@
 <template>
 	<div class="flex-1 flex flex-col items-stretch min-h-0 p-4 space-y-4">
-		<div class="w-full flex flex-row space-x-2">
-			<div
-				class="h-10 flex flex-row rounded-md items-center cursor-pointer bg-plastic-800 ring-2 ring-plastic-900">
+		<div class="w-full flex flex-row space-x-2 items-center">
+			<div class="flex flex-row rounded-md items-center cursor-pointer bg-plastic-800 ring-2 ring-plastic-900">
 				<ViewGridIcon
-					class="w-10 p-2 rounded-md text-plastic-200 border-y border-t-amber-600 border-b-amber-900 bg-gradient-to-b from-amber-800 to-amber-600" />
-				<ViewListIcon class="w-10 p-2 text-plastic-700" />
+					class="w-9 p-2 rounded-md text-plastic-200 border-y border-t-amber-600 border-b-amber-900 bg-gradient-to-b from-amber-800 to-amber-600" />
+				<ViewListIcon class="w-9 p-2 text-plastic-700" />
 			</div>
 			<div
 				class="flex-1 rounded-md overflow-hidden bg-plastic-800 border-y border-plastic-900 border-b-plastic-600">
-				<input type="text" placeholder="Filter Frames"
-					class="w-full h-[2.55rem] px-4 font-medium bg-transparent border-0" />
+				<input type="text" placeholder="Filter Frames" class="w-full px-4 bg-transparent border-0" />
 			</div>
 			<Button :positive="true" icon="PhotographIcon" label="Import" @click="importFrames" />
 		</div>
