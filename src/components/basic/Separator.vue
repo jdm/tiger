32acaa8412a52d83808505e26953ce533e13fa96
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-30 00:33:18 -0700

    Support vertical separators

--- src/components/basic/Separator.vue
+++ src/components/basic/Separator.vue
@@ -1,3 +1,10 @@
 <template>
-	<div class="h-0 border-y border-t-plastic-600 border-b-plastic-800" />
+	<div class="">
+		<div
+			:class="vertical ? 'w-0 h-full border-x border-l-plastic-600 border-r-plastic-800' : 'h-0 w-full border-y border-t-plastic-600 border-b-plastic-800'" />
+	</div>
 </template>
+
+<script setup lang="ts">
+defineProps<{ vertical?: boolean }>();
+</script>
