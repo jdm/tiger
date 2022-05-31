9cccc71a44864d288efa7d8b350ed19f86deabc1
Antoine Gersant <antoine.gersant@lesforges.org>
2022-05-30 19:35:31 -0700

    Color changes

--- src/components/basic/MultiSwitch.vue
+++ src/components/basic/MultiSwitch.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="relative rounded-md bg-plastic-800 border-2 border-plastic-900">
 		<div v-if="activeItemElement" :style="knobStyle"
-			class="absolute top-0 w-9 h-9 rounded-md transition-[left] border-y border-t-cyan-600 border-b-cyan-900 bg-gradient-to-b from-cyan-800 to-cyan-600" />
+			class="absolute top-0 w-9 h-9 rounded-md transition-[left] border-y border-t-amber-600 border-b-amber-900 bg-gradient-to-b from-amber-800 to-amber-600" />
 		<div ref="itemsElement" class="flex flex-row">
 			<div v-for="item in items" class="z-10">
 				<Icon :name="item.icon" @click="onItemClicked(item)" class="w-9 p-2 transition"
@@ -50,7 +50,7 @@ const knobStyle = computed(() => {
 
 function itemClasses(item: MultiSwitchItem) {
 	return [
-		...(item.active ? ["text-cyan-200"] : ["text-plastic-500", "cursor-pointer"]),
+		...(item.active ? ["text-amber-200"] : ["text-plastic-500", "cursor-pointer"]),
 		...(item.rotate ? ["rotate-45"] : [])
 	];
 }
