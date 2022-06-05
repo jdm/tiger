6e089336ba9d9c66f6a11a2314b625da69723d0b
Antoine Gersant <antoine.gersant@lesforges.org>
2022-06-04 17:47:53 -0700

    Cosmetic changes

--- src/components/workbench/Frame.vue
+++ src/components/workbench/Frame.vue
@@ -1,13 +1,12 @@
 <template>
-	<img ref="el" :src="convertFileSrc(keyframe.frame)" @load="onFrameLoaded"
-		class="absolute pixelated transition-transform" :style="frameStyle(keyframe)" />
+	<img ref="el" :src="convertFileSrc(keyframe.frame)" @load="onFrameLoaded" class="pixelated" :style="frameStyle" />
 </template>
 
 <script setup lang="ts">
 import { convertFileSrc } from '@tauri-apps/api/tauri'
 import { Keyframe } from '@/api/dto'
 import { useAppStore } from '@/stores/app'
-import { ref, Ref } from 'vue';
+import { computed, CSSProperties, ref, Ref } from 'vue';
 
 const app = useAppStore();
 
@@ -18,27 +17,38 @@ const props = defineProps<{
 
 const el: Ref<HTMLImageElement | null> = ref(null);
 const frameSize: Ref<[number, number] | null> = ref(null);
+const isActiveFrame = computed(() => props.keyframe == app.currentKeyframe);
+
+const positioning = computed((): CSSProperties => {
+	const zoom = app.currentDocument?.workbenchZoom || 1;
+	const size: [number, number] = frameSize.value || [0, 0];
+	const left = props.origin[0] - Math.floor(size[0] / 2) + props.keyframe.offset[0];
+	const top = props.origin[1] - Math.floor(size[1] / 2) + props.keyframe.offset[1];
+	const width = size[0];
+	const height = size[1];
+	const transformOrigin = [props.origin[0] - left, props.origin[1] - top];
+	return {
+		position: "absolute",
+		left: left + "px",
+		top: top + "px",
+		width: width + "px",
+		height: height + "px",
+		transform: "scale(" + zoom + "," + zoom + ")",
+		transformOrigin: transformOrigin[0] + "px " + transformOrigin[1] + "px",
+	};
+});
+
+const frameStyle = computed(() => {
+	return {
+		...positioning.value,
+		opacity: frameSize.value && isActiveFrame.value ? 1 : 0,
+	};
+});
 
 function onFrameLoaded() {
 	if (!el.value) {
 		return;
 	}
 	frameSize.value = [el.value.naturalWidth, el.value.naturalHeight];
-}
-
-function frameStyle(keyframe: Keyframe) {
-	const zoom = app.currentDocument?.workbenchZoom || 1;
-	const opacity = frameSize.value && keyframe == app.currentKeyframe ? 1 : 0;
-	const size: [number, number] = frameSize.value || [0, 0];
-	const left = props.origin[0] - Math.floor(size[0] / 2) + keyframe.offset[0];
-	const top = props.origin[1] - Math.floor(size[1] / 2) + keyframe.offset[1];
-	const transformOrigin = [props.origin[0] - left, props.origin[1] - top];
-	return {
-		opacity: opacity,
-		left: left + "px",
-		top: top + "px",
-		transform: "scale(" + zoom + "," + zoom + ")",
-		transformOrigin: transformOrigin[0] + "px " + transformOrigin[1] + "px",
-	};
 }
 </script>
