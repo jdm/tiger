13ee2873f64320ddc9254c0d92a2c6c2792f5578
Antoine Gersant <antoine.gersant@lesforges.org>
2022-06-08 21:32:24 -0700

    Removed placeholder icon

--- src/components/keyframe/KeyframePane.vue
+++ src/components/keyframe/KeyframePane.vue
@@ -8,7 +8,6 @@
 		<div class="flex-1 flex flex-col min-h-0 p-4 space-y-4">
 			<div class="w-full flex flex-row space-x-2 items-center">
 				<Toggle :toggled="true" icon="EyeIcon" />
-				<Toggle :toggled="false" icon="LockClosedIcon" />
 				<div class="flex-1 flex flex-row justify-end">
 					<Button :positive="true" icon="TagIcon" label="Add" />
 				</div>
