b5c1ec02c7a3d8c5f7a8d2b2832b1c48eed1537c
Antoine Gersant <antoine.gersant@lesforges.org>
2022-06-13 20:37:10 -0700

    Can use system dialog to pick export settings

--- src/components/basic/InputPath.vue
+++ src/components/basic/InputPath.vue
@@ -1,8 +1,8 @@
 <template>
 	<InputText v-model="value">
 		<template #after>
-			<span class="inline-flex items-center px-3 rounded-md
-				bg-plastic-600
+			<span @click="openFilePicker" class="inline-flex items-center px-3 rounded-md
+				cursor-pointer  bg-plastic-600
 				border border-l-0 border-t-plastic-500 border-b-plastic-900 border-r-plastic-800">
 				<FolderIcon v-if="isDirectory" class="w-5 h-5 text-plastic-300" />
 				<DotsHorizontalIcon v-if="!isDirectory" class="w-5 h-5 text-plastic-300" />
@@ -13,6 +13,7 @@
 
 
 <script setup lang="ts">
+import { open } from "@tauri-apps/api/dialog";
 import { computed } from "vue";
 import { DotsHorizontalIcon, FolderIcon } from "@heroicons/vue/solid"
 import InputText from "@/components/basic/InputText.vue"
@@ -32,5 +33,12 @@ const value = computed({
 		emit("update:modelValue", value);
 	}
 });
+
+async function openFilePicker() {
+	const file = await open({ directory: props.isDirectory });
+	if (typeof file === "string") {
+		value.value = file;
+	}
+}
 
 </script>
