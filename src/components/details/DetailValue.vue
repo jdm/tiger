3c7079a266b1693bae529e7a356324e40a9b2d94
Antoine Gersant <antoine.gersant@lesforges.org>
2022-06-20 16:11:02 -0700

    Details pane iteration

--- src/components/details/DetailValue.vue
+++ src/components/details/DetailValue.vue
@@ -1,6 +1,13 @@
 <template>
-	<div class="self-center outline outline-1 outline-plastic-700 rounded-sm">
-		<input class="w-full bg-plastic-900 text-xs text-plastic-300 rounded-sm border-0 focus:ring-0" type="text"
-			spellcheck="false" value="48 px" />
+	<div class="self-center flex flex-row bg-plastic-900 outline outline-1 outline-plastic-700 rounded-sm">
+		<input class="w-full  text-xs bg-transparent text-plastic-300 rounded-sm border-0 focus:ring-0" type="text"
+			spellcheck="false" value="48" />
+		<div v-if="unit" class="text-xs self-center px-2 text-plastic-500">{{ unit }}</div>
 	</div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+	unit?: string,
+}>();
+</script>
--- src/components/details/DetailsPane.vue
+++ src/components/details/DetailsPane.vue
@@ -3,19 +3,16 @@
 		<div class="flex flex-row bg-plastic-900">
 			<PaneTab :selected="true">Details</PaneTab>
 		</div>
-		<PaneInset class="h-full m-4 p-4">
-			<div class="h-full px-4 grid grid-cols-10">
-				<DetailKey class="col-span-3">Name</DetailKey>
-				<DetailValue class="col-span-7" />
+		<PaneInset class="m-4 p-4">
+			<div class="px-4 grid grid-cols-10 gap-y-2">
+				<DetailKey class="col-span-4">X</DetailKey>
+				<DetailValue class="col-span-6" unit="px" />
 
-				<DetailKey class="col-span-3">X</DetailKey>
-				<DetailValue class="col-span-7" />
+				<DetailKey class="col-span-4">Y</DetailKey>
+				<DetailValue class="col-span-6" unit="px" />
 
-				<DetailKey class="col-span-3">Y</DetailKey>
-				<DetailValue class="col-span-7" />
-
-				<DetailKey class="col-span-3">Width</DetailKey>
-				<DetailValue class="col-span-5" />
+				<DetailKey class="col-span-4">Width</DetailKey>
+				<DetailValue class="col-span-4" unit="px" />
 
 				<div class="col-span-2 row-span-2 flex flex-col justify-center pl-2">
 					<div class="h-2.5 w-1/2 border-t border-r border-plastic-500" />
@@ -23,8 +20,8 @@
 					<div class="h-2.5 w-1/2 border-b border-r border-plastic-500" />
 				</div>
 
-				<DetailKey class="col-span-3">Height</DetailKey>
-				<DetailValue class="col-span-5 " />
+				<DetailKey class="col-span-4">Height</DetailKey>
+				<DetailValue class="col-span-4" unit="px" />
 			</div>
 		</PaneInset>
 	</Pane>
