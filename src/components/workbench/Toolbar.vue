ef9b557c60d5b96896d35e1a5ee12756dc531f57
Antoine Gersant <antoine.gersant@lesforges.org>
2022-06-19 18:43:55 -0700

    Toolbar style update

--- src/components/workbench/Toolbar.vue
+++ src/components/workbench/Toolbar.vue
@@ -1,25 +1,25 @@
 <template>
 	<div class="p-2 px-4
 		flex flex-row space-x-10 items-center
-		rounded-md bg-plastic-900
-		border-2 border-plastic-700
-		outline outline-4 outline-plastic-900
+		rounded-md bg-neutral-900
+		border-2 border-neutral-800
+		outline outline-4 outline-neutral-900
 	">
-		<Toggle :toggled="!app.currentDocument?.darkenSprites" @click="toggleSpriteDarkening" icon="SunIcon" />
+		<!-- <Toggle :toggled="!app.currentDocument?.darkenSprites" @click="toggleSpriteDarkening" icon="SunIcon" /> -->
+		<Icon name="SunIcon" class="w-9 h-9 text-plastic-400 p-2 rounded-md bg-plastic-700" />
 		<div class="flex flex-row items-center font-xs font-semibold">
 			<div
-				class="w-11 h-11 inline-flex items-center justify-center text-plastic-200 border-2 border-blue-600 rounded-md">
+				class="w-10 h-9 inline-flex items-center justify-center text-plastic-200 border-2 border-blue-600 rounded-md">
 				1x</div>
-			<div class="w-11 h-11 flex items-center justify-center text-plastic-400"><span>2x</span></div>
-			<div class="w-11 h-11 flex items-center justify-center text-plastic-400 "><span>4x</span></div>
-			<div class="w-11 h-11 flex items-center justify-center text-plastic-400 "><span>8x</span></div>
-			<div class="w-11 h-11 flex items-center justify-center text-plastic-400 "><span>16x</span></div>
+			<div class="w-10 h-9 flex items-center justify-center text-plastic-400"><span>2x</span></div>
+			<div class="w-10 h-9 flex items-center justify-center text-plastic-400 "><span>4x</span></div>
+			<div class="w-10 h-9 flex items-center justify-center text-plastic-400 "><span>8x</span></div>
+			<div class="w-10 h-9 flex items-center justify-center text-plastic-400 "><span>16x</span></div>
 		</div>
 		<div class="flex flex-row items-center space-x-1.5">
-			<Icon name="PhotographIcon" class="w-9 h-9 text-blue-200 p-2 rounded-md bg-blue-600" />
-			<Icon name="TagIcon" class="w-9 h-9 text-blue-200 p-2 rounded-md bg-blue-600" />
-			<Icon name="PlusIcon" class="w-9 h-9 text-plastic-200 p-2 rounded-md bg-plastic-700" />
-			<Icon name="LocationMarkerIcon" class="w-9 h-9 text-plastic-200 p-2 rounded-md bg-plastic-700" />
+			<Icon name="PhotographIcon" class="w-9 h-9 text-orange-200 p-2 rounded-md bg-orange-700" />
+			<Icon name="TagIcon" class="w-9 h-9 text-pink-200 p-2 rounded-md bg-pink-700" />
+			<Icon name="PlusIcon" class="w-9 h-9 text-sky-200 p-2 rounded-md bg-sky-600" />
 		</div>
 	</div>
 </template>
