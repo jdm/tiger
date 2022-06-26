64c02260c9a17308522626244645bef404c4f6ba
Antoine Gersant <antoine.gersant@lesforges.org>
2022-06-25 20:39:06 -0700

    Release process WIP

--- make_release.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem "Cargo.toml" | ForEach-Object {
-	$conf = $_ | Get-Content -raw
-	$conf -match 'version\s+=\s+"(.*)"' | out-null
-	$script:TIGER_VERSION = $matches[1]
-}
-
-git tag -a $TIGER_VERSION -m "Tiger $TIGER_VERSION"
-git push origin $TIGER_VERSION
