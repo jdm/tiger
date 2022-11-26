1c6116df0eb611f95076680c33c7aa37e0ea37c5
Antoine Gersant <antoine.gersant@lesforges.org>
2022-11-26 13:34:33 -0800

    Can right click in animations panel to paste

--- src/components/animations/Animations.vue
+++ src/components/animations/Animations.vue
@@ -5,25 +5,33 @@
 			<Button :positive="true" icon="FilmIcon" label="New" @click="createAnimation" />
 		</div>
 		<PaneInset class="flex-1 min-h-0">
-			<div class="p-4 overflow-y-auto h-full styled-scrollbars">
+			<div class="p-4 overflow-y-auto h-full styled-scrollbars" @contextmenu.stop.prevent="onOpenContextMenu">
 				<div class="flex flex-col">
 					<Animation v-for="animation in visibleAnimations" :animation="animation" :key="animation.name" />
 				</div>
+				<ContextMenu ref="contextMenu" :content="contextMenuEntries" />
 			</div>
 		</PaneInset>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue"
-import { createAnimation, filterAnimations } from "@/api/document"
+import { computed, Ref, ref } from "vue"
+import { createAnimation, filterAnimations, paste } from "@/api/document"
+import { ClipboardManifest } from "@/api/dto"
 import { useAppStore } from "@/stores/app"
 import Animation from "@/components/animations/Animation.vue"
 import Button from "@/components/basic/Button.vue"
+import ContextMenu from "@/components/basic/ContextMenu.vue"
 import InputSearch from "@/components/basic/InputSearch.vue"
 import PaneInset from "@/components/basic/PaneInset.vue"
 
 const app = useAppStore();
+const contextMenu: Ref<typeof ContextMenu | null> = ref(null);
+
+const contextMenuEntries = computed(() => [
+	{ name: "Paste", shortcut: "Ctrl+V", action: paste, disabled: app.clipboardManifest != ClipboardManifest.Animations },
+]);
 
 const visibleAnimations = computed(() => {
 	return app.sortedAnimations?.filter((a) => !a.filteredOut);
@@ -33,4 +41,10 @@ const searchQuery = computed({
 	get: () => app.currentDocument?.animationsFilter || "",
 	set: filterAnimations,
 });
+
+function onOpenContextMenu(event: MouseEvent) {
+	if (contextMenu.value) {
+		contextMenu.value.show(event);
+	}
+}
 </script>
