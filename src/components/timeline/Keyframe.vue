7452831372f7d628ed8936b458cdda0b3a6dcb16
Antoine Gersant <antoine.gersant@lesforges.org>
2022-11-27 16:29:06 -0800

    Fixed a bug where clicking keyframe resize handle would select first frame in that sequence

--- src/components/timeline/Keyframe.vue
+++ src/components/timeline/Keyframe.vue
@@ -1,8 +1,8 @@
 <template>
-	<div class="pr-1">
+	<div class="pr-1" @click.stop @dblclick.stop @contextmenu.stop>
 		<div ref="el" @dragstart="onDragStart" @dragend="onDragEnd" draggable="true"
 			class="h-full min-w-0 relative rounded-md border-2 cursor-pointer" :class="dynamicClasses">
-			<div @click.stop="onKeyframeClicked" @dblclick.stop @contextmenu.stop.prevent="onOpenContextMenu"
+			<div @click="onKeyframeClicked" @contextmenu.prevent="onOpenContextMenu"
 				class="h-full px-2 flex items-center font-semibold text-[11px]">
 				<div class="min-w-0 overflow-hidden whitespace-nowrap text-ellipsis">{{ name }}</div>
 			</div>
