5e45caff158442aad48f2b533ac5c92c336e2466
Antoine Gersant <antoine.gersant@lesforges.org>
2023-01-22 20:07:18 -0800

    Fixed unreadable signature asset

--- src-tauri/distribution/generate-update-manifest.ps1
+++ src-tauri/distribution/generate-update-manifest.ps1
@@ -1,5 +1,10 @@
+if (-not (Test-Path -Path env:\GITHUB_TOKEN)) {
+	throw 'GITHUB_TOKEN environment variable is not defined'
+}
+$github_token = $env:GITHUB_TOKEN
+
 if (-not (Test-Path -Path env:\WINDOWS_ASSETS)) {
-	throw 'RELEASE_ASSETS environment variable is not defined'
+	throw 'WINDOWS_ASSETS environment variable is not defined'
 }
 $windows_assets = $env:WINDOWS_ASSETS
 
@@ -8,19 +13,23 @@ if (-not (Test-Path -Path env:\TIGER_VERSION)) {
 }
 $tiger_version = $env:TIGER_VERSION
 
-$msi_signature_url = $windows_assets
+
+$msi_signature_asset_id = $windows_assets
 | ConvertFrom-Json
-| Where-Object { $_.browser_download_url -match '\.msi\.zip\.sig$' }
-| Select-Object -ExpandProperty browser_download_url
-$msi_signature = (Invoke-webrequest -URI $msi_signature_url).Content
+| Where-Object { $_.name -match '\.msi\.zip\.sig$' }
+| Select-Object -ExpandProperty id
+$msi_signature_draft_url = 'https://api.github.com/repos/agersant/tiger/releases/assets/' + $msi_signature_asset_id
+$msi_signature = (Invoke-webrequest -Headers @{'Accept' = 'application/octet-stream'; 'Authorization' = $github_token } -URI $msi_signature_draft_url).Content
 if ($msi_signature.GetType().IsArray) {
 	$msi_signature = [System.Text.Encoding]::ASCII.GetString($msi_signature);
 }
 
-$msi_updater_url = $windows_assets
+$msi_updater_name = $windows_assets
 | ConvertFrom-Json `
-| Where-Object { $_.browser_download_url -match '\.msi\.zip$' }
-| Select-Object -ExpandProperty browser_download_url
+| Where-Object { $_.name -match '\.msi\.zip$' }
+| Select-Object -ExpandProperty name
+# Cannot use the browser_download_url property for this asset as it changes when publishing the draft
+$msi_updater_url = 'https://github.com/agersant/tiger/releases/download/' + $tiger_version + '/' + $msi_updater_name
 
 $manifest = Get-Content -Path 'src-tauri/distribution/update-manifest-template.json'
 $manifest = $manifest -replace '{TIGER_VERSION}', $tiger_version
