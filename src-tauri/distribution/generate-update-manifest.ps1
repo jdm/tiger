527e22661b2d4cc29ce99bcd64ea7da1ebb9037d
Antoine Gersant <antoine.gersant@lesforges.org>
2023-01-22 17:32:03 -0800

    Fix missing asset list

--- src-tauri/distribution/generate-update-manifest.ps1
+++ src-tauri/distribution/generate-update-manifest.ps1
@@ -1,26 +1,26 @@
-if (-not (Test-Path -Path env:\RELEASE_ASSETS)) {
+if (-not (Test-Path -Path env:\WINDOWS_ASSETS)) {
 	throw 'RELEASE_ASSETS environment variable is not defined'
 }
-$release_assets = $env:RELEASE_ASSETS
+$windows_assets = $env:WINDOWS_ASSETS
 
 if (-not (Test-Path -Path env:\TIGER_VERSION)) {
 	throw 'TIGER_VERSION environment variable is not defined'
 }
 $tiger_version = $env:TIGER_VERSION
 
-$signature_url = $release_assets
+$msi_signature_url = $windows_assets
 | ConvertFrom-Json
 | Where-Object { $_.browser_download_url -match '\.msi\.zip\.sig$' }
 | Select-Object -ExpandProperty browser_download_url
-$signature = (Invoke-webrequest -URI $signature_url).Content
+$msi_signature = (Invoke-webrequest -URI $msi_signature_url).Content
 
-$updater_url = $release_assets
+$msi_updater_url = $windows_assets
 | ConvertFrom-Json `
 | Where-Object { $_.browser_download_url -match '\.msi\.zip$' }
 | Select-Object -ExpandProperty browser_download_url
 
 $manifest = Get-Content -Path 'src-tauri/distribution/update-manifest-template.json'
 $manifest = $manifest -replace '{TIGER_VERSION}', $tiger_version
-$manifest = $manifest -replace '{MSI_SIGNATURE}', $signature
-$manifest = $manifest -replace '{MSI_URL}', $updater_url
+$manifest = $manifest -replace '{MSI_SIGNATURE}', $msi_signature
+$manifest = $manifest -replace '{MSI_URL}', $msi_updater_url
 $manifest | Set-Content -Path 'update-manifest.json'
